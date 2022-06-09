445100e44c753eee8276aeac0838442b63558e65
Komal-Jayswal <Komal.Jayswal@maersk.com>
2022-06-09 21:42:06 +0530

    first commit

---
+++ src/main/java/net/learning/IntegrateCustomException/IntegrateCustomExceptionApplication.java
@@ -0,0 +1,13 @@
+package net.learning.IntegrateCustomException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateCustomExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrateCustomExceptionApplication.class, args);
+	}
+
+}
